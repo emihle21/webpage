b6d0cb942190a9ad763d0bf2bb34ecef8fa5328e
Thokozile Magwaza <thokozilembamagwaza@gmail.com>
2023-02-28 22:57:32 +0200

    fetching data from js

---
+++ assets/mydata/portfolio.js
@@ -0,0 +1,46 @@
+//personal information
+
+document.getElementById("firstname").innerHTML ='Thokozile';
+document.getElementById("secondname").innerHTML ='Mbally';
+document.getElementById("surname").innerHTML ='Magwaza';
+
+document.getElementById("Date of Birth").innerHTML ='25/12/1995';
+document.getElementById("Marital Status").innerHTML ='Single';
+document.getElementById("Nationality").innerHTML ='South African';
+document.getElementById("Identity Number ").innerHTML ='9512250807083';
+
+
+document.getElementById("mybio").innerHTML ='I am a hard working, honest individual. I am a good timekeeper, always willing to learn new skills. I am friendly,'+
+'helpful and polite, have a good sense of humour. I am able to work independently in busy environments and'+
+'also within a team setting. I am outgoing and tactful, and able to listen effectively when solving problems. And'+
+'my main objective is to obtain a job within my chosen field that will challenge me and allow me to use my'+
+'education and skills in a way that is mutually beneficial to both myself and my employer and allow for future'+
+'growth and advancement.';
+
+//contatct information
+
+document.getElementById("email").innerHTML ='thokozilembamagwaza@gmail.com';
+document.getElementById("phone").innerHTML ='0796610451';
+document.getElementById("address").innerHTML ='62 Lehabe Street Attreridgville 0008';
+document.getElementById("github").innerHTML ='https://github.com/emihle21';
+
+
+//education
+
+
+document.getElementById("Schoolname").innerHTML ='Umzikazi High School';
+document.getElementById("qualification").innerHTML ='Matric';
+document.getElementById("year").innerHTML ='2013';
+document.getElementById("status").innerHTML ='Completed';
+
+document.getElementById("Schoolname1").innerHTML ='Tshwane University of Technology';
+document.getElementById("qualification1").innerHTML ='National Diploma in information Technology';
+document.getElementById("status1").innerHTML ='In progress';
+
+
+
+
+
+
+//projects
+const projects = ["Blind System(School project)"];
